37cb180a86a57740011bd494bc572bd105a5bffa
juhanikat <juhani.kataja@gmail.com>
2023-05-15 14:27:14 +0300

    done up to 5.1

--- blogilista/src/App.js
+++ blogilista/src/App.js
@@ -1,24 +1,79 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from "./services/login"
+import Notification from './components/Notification'
+import Error from './components/Error'
+import LoginForm from './components/Login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [notificationMessage, setNotificationMessage] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password, })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <Notification message={notificationMessage} />
+        <Error message={errorMessage} />
+        <div>
+          <h2>Log in to application</h2>
+          <LoginForm
+            onSubmit={handleLogin}
+            username={username}
+            password={password}
+            setUsername={setUsername}
+            setPassword={setPassword}
+          />
+        </div>
+        <div>
+          <h2>blogs</h2>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <Notification message={notificationMessage} />
+      <Error message={errorMessage} />
+      <div>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
     </div>
   )
 }
-
 export default App
---
+++ blogilista/src/components/Login.js
@@ -0,0 +1,27 @@
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.onSubmit}>
+            <div>
+                name: <input
+                    type="text"
+                    name="Username"
+                    value={props.username}
+                    onChange={({ target }) => props.setUsername(target.value)}
+                />
+            </div>
+            <div>
+                password: <input
+                    type="text"
+                    name="Password"
+                    value={props.password}
+                    onChange={({ target }) => props.setPassword(target.value)}
+                />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+export default LoginForm
